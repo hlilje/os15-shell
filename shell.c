657df919a3a0397e3bd017c848593bfc6f682d99
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-04-16 11:06:55 +0200

    Add read input loop

--- shell.c
+++ shell.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 #include "shell.h"
 
-int main(void)
+int main(int argc, const char* argv[])
 {
-    printf("shell\n");
+    for (;;)
+    {
+        char str[80]; /* Fixed length */
+        int i;
+
+        fgets(str, 80, stdin);
+
+        /* Remove newline, if present */
+        i = strlen(str) - 1;
+        if(str[ i ] == '\n') str[i] = '\0';
+
+        printf("%s\n", str);
+    }
+
+    return 0;
 }
