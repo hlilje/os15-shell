f54fa1c9341a9900c558869ac7bebd80d77b1bfe
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-04-16 14:30:11 +0200

    Add read_cmd function

--- shell.c
+++ shell.c
@@ -1,34 +1,47 @@
 #include "shell.h"
 
+
+int read_cmd(char* cmd, const char* input, int i)
+{
+    /* Read one command */
+    int j;
+    for (j = 0; ; ++i, ++j)
+    {
+        cmd[j] = input[i];
+        /* TODO Check for escaped spaces */
+        if (input[i] == ' ' || input[i] == '\0')
+        {
+            /* Let i be the next non-space character */
+            while (input[i] == ' ') ++i;
+            cmd[j] = '\0';
+            break;
+        }
+    }
+
+    return i;
+}
+
 int main(int argc, const char* argv[])
 {
-    for (;;)
+    while(1)
     {
-        char input[80], cmd[80];
-        int i, j;
+        char input[80], cmd[80], buf[100];
+        int i;
 
         printf("> "); /* Prompt */
 
         /* Exit if error occurs */
-        if(!fgets(input, 80, stdin)) exit(1);
+        if (!fgets(input, 80, stdin)) exit(1);
 
         /* Remove newline, if present */
         i = strlen(input) - 1;
         if (input[i] == '\n') input[i] = '\0';
 
         /* Read given commands */
-        for (i = 0;; ++i, ++j) /* Increment to skip space */
+        for(i = 0;;)
         {
             /* Read one command */
-            for (j = 0; ; ++i, ++j)
-            {
-                cmd[j] = input[i];
-                if (input[i] == ' ' || input[i] == '\0')
-                {
-                    cmd[j] = '\0';
-                    break;
-                }
-            }
+            i = read_cmd(cmd, input, i);
 
             if (strcmp(cmd, "exit") == 0)
             {
@@ -39,6 +52,7 @@ int main(int argc, const char* argv[])
             else if (strcmp(cmd, "cd") == 0)
             {
                 printf("cd\n");
+                printf("%s\n", getwd(buf));
             }
             else if (strcmp(cmd, "checkEnv") == 0)
             {
--- shell.h
+++ shell.h
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+
+/**
+ * Read one command from input into cmd.
+ * Return the new index i.
+ */
+int read_cmd(char* cmd, const char* input, int i);
